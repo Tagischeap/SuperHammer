b4fb5f0bb624f4da2ff7b50ae531c70819efa954
Taggy <Tagischeap@Gmail.com>
2023-11-26 01:10:06 -0800

    More stuff
    Added another Swing
    Added Momentum to Swings
    Swinging doesn't feel quiet right.

--- Scenes/Player/player.gd
+++ Scenes/Player/player.gd
@@ -1,23 +1,32 @@
 extends CharacterBody2D
 
 enum States {MOVE, JUMP, SWINGX, SWINGY}
+enum LegStates {IDLE, MOVE, JUMP, FALL}
+enum ArmStates {IDLE, SWINGX, SWINGY}
+enum DirectionStates {LEFT, RIGHT}
+
+var CurrentLegState = LegStates.IDLE
+var CurrentArmState = ArmStates.IDLE
+var CurrentDirectionState = DirectionStates.RIGHT
+
 var CurrentState = States.MOVE
 
 const SPEED = 125.0
-const JUMP_VELOCITY = -200.0
-var pressed = 2
-
+const JUMP_VELOCITY = -250.0
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 func _ready():
 	$Hammer/CollisionShape2D.disabled = true
+	
 func _physics_process(delta):
 	match CurrentState:
 		States.MOVE:
 			Move(delta)
 		States.SWINGX:
 			SwingX()
+		States.SWINGY:
+			SwingY()
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
@@ -30,18 +39,10 @@ func Move(delta):
 			$AnimationPlayer.play("Fall")
 	else:
 		if velocity.x == 0: 
-			pressed = 2
 			$AnimationPlayer.play("Idle")
 	# Handle Jump.
-	if Input.is_action_just_pressed("ui_select"):
-		pressed -= 1
-		
 	if Input.is_action_just_pressed("ui_select") and is_on_floor():
-		if pressed > 0:
-			Jump()
-	if Input.is_action_just_pressed("ui_select"):
-		if pressed > 0:
-			Jump()
+		Jump()
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -50,16 +51,19 @@ func Move(delta):
 		velocity.x = direction * SPEED
 		$AnimationPlayer.play("Walk")
 		if direction > 0:
-			$Sprite2D.flip_h = false
-			$Hammer/CollisionShape2D.position.x = 28
+			ChangeDirection(DirectionStates.RIGHT)
 		else:
-			$Sprite2D.flip_h = true
-			$Hammer/CollisionShape2D.position.x = -28
+			ChangeDirection(DirectionStates.LEFT)
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-	
+		
 	if Input.is_action_just_pressed("ui_action"):
 			CurrentState = States.SWINGX
+	if Input.is_action_just_pressed("ui_action2"):
+			CurrentState = States.SWINGY
+
+func PushPlayer(dir):
+	velocity += dir
 	
 func Jump():
 		$AnimationPlayer.play("Jump")
@@ -67,6 +71,34 @@ func Jump():
 
 func SwingX():
 	$AnimationPlayer.play("Swing X")
-	
+func ShoveX():
+	if CurrentDirectionState == DirectionStates.RIGHT:
+		PushPlayer(Vector2(300,-100))
+	else:
+		PushPlayer(Vector2(-300,-100))
+		
+func SwingY():
+	$AnimationPlayer.play("Swing Y")
+func ShoveY():
+	if CurrentDirectionState == DirectionStates.RIGHT:
+		PushPlayer(Vector2(100,-250))
+	else:
+		PushPlayer(Vector2(-100,-250))
+		
+func ChangeDirection(dir):
+	if dir == null:
+		if CurrentDirectionState == DirectionStates.RIGHT:
+			dir = DirectionStates.RIGHT
+		else:
+			dir = DirectionStates.LEFT
+	if dir == DirectionStates.RIGHT:
+			CurrentDirectionState = DirectionStates.RIGHT
+			$Sprite2D.flip_h = false
+			$Hammer/CollisionShape2D.position.x = 28
+	elif dir == DirectionStates.LEFT:
+			CurrentDirectionState = DirectionStates.LEFT
+			$Sprite2D.flip_h = true
+			$Hammer/CollisionShape2D.position.x = -28
+
 func OnStateFinished():
 	CurrentState = States.MOVE
