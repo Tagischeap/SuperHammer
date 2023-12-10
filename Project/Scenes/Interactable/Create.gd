ea4eed62621ec8f8448fa3cf20ebb20cd12bef90
Taggy <Tagischeap@Gmail.com>
2023-12-10 04:08:00 -0800

    Made crate my own
    Also fixed the issue with the files where I had the project folder in the root folder.

--- Project/Scenes/Interactable/Create.gd
+++ Project/Scenes/Interactable/Create.gd
@@ -1,14 +1,32 @@
-extends StaticBody2D
+extends CharacterBody2D
 
 var health = 3
 
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
 func _on_hitbox_area_entered(area):
 	if area.name == "HurtBox":
-		$AnimationPlayer.play("Hurt")
+		if health == 3:
+			$AnimationPlayer.play("Hurt")
+		if health == 2:
+			$AnimationPlayer.play("Hurt 2")
+		if health == 1:
+			$AnimationPlayer.play("Hurt 3")
+
 		health -= 1
 		await $AnimationPlayer.animation_finished
-		$AnimationPlayer.play("Active")
 	if health <= 0:
 		$AnimationPlayer.play("Destroyed")
 		await $AnimationPlayer.animation_finished
 		queue_free()
+		
+func _physics_process(delta):
+	floor_detection(delta)
+func floor_detection(delta):
+	if is_on_floor():
+		pass
+	else:
+		# Add the gravity.
+		velocity.y += gravity * delta
+		
